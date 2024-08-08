3574c99a654019e3831ad639142c020bebe91409
Bastien Guerry <bastien.guerry@code.gouv.fr>
2024-08-08 18:27:42 +0200

    src/convert.clj: Update

--- src/convert.clj
+++ src/convert.clj
@@ -1,5 +1,8 @@
 #!/usr/bin/env bb
 
+;; Remove output file if left over
+(fs/delete-if-exists "output.yml")
+
 ;; Get all forges by domain names
 (def orgs
   (->>
@@ -8,22 +11,27 @@
 
 (defn spt! [s] (spit "output.yml" s :append true))
 
-(defn write-props! [forge]
-  (doseq [[group properties] (second forge)]
-    (when-let [g (last (re-find #"https://[^/]+/([^:]+)" group))]
-      (spt! (str "    " g ":\n"))
-      (when-let [pso (first (get properties "service_of"))]
-        (spt! (str "      pso: " pso "\n")))
-      (when-let [pso_id (first (get properties "pso_id"))]
-        (spt! (str "      pso_id: " pso_id "\n")))
-      (when-let [floss (first (get properties "floss_policy"))]
-        (spt! (str "      floss_policy: " (first (get properties "floss_policy")) "\n"))))))
+(def non-purpose-forges
+  '("github.com" "gitlab.com" "sr.ht" "framagit.org" "gitlab.adullact.net" "gitlab.ow2.org"))
+
+(defn write-props! [forge & without-groups?]
+  (let [re (if without-groups? #"https://([^/]+)" #"https://[^/]+/([^:]+)")]
+    (doseq [[group properties] (second forge)]
+      (when-let [g (last (re-find re group))]
+        (when-not without-groups? (spt! (str "    " g ":\n")))
+        (let [indent (if without-groups? "  " "      ")]
+          (when-let [pso (first (get properties "service_of"))]
+            (spt! (str indent "pso: " pso "\n")))
+          (when-let [pso_id (first (get properties "pso_id"))]
+            (spt! (str indent "pso_id: " pso_id "\n")))
+          (when-let [floss (first (get properties "floss_policy"))]
+            (spt! (str indent "floss_policy: " (first (get properties "floss_policy")) "\n"))))))))
 
 (doseq [forge orgs]
   ;; For each forge, spit its domain name:
   (spt! (str "\n" (first forge) ":\n"))
   ;; For each general purpose forge, list groups as such:
-  (if-let [n (re-find #"github.com|gitlab.com|sr.ht|framagit.org|gitlab.adullact.net|gitlab.ow2.org" (first forge))]
+  (if-let [n (re-find (re-pattern (str/join "|" non-purpose-forges)) (first forge))]
     (do (spt! "  general_purpose: true\n")
         (spt! (str "  forge: " (condp = n
                                  "github.com" "github"
@@ -31,5 +39,6 @@
                                  "gitlab") "\n"))
         (spt! "  groups:\n")
         (write-props! forge))
+    ;; Assume other non general_purpose forges are GitLab for now
     (do (spt! "  forge: gitlab\n")
-        (write-props! forge))))
+        (write-props! forge true))))
